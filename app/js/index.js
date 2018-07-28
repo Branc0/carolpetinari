a9557e6f4b2203323b6f9688dbc0c46e93e528e4
branc0 <rrochadeazevedo@gmail.com>
2018-07-28 20:35:36 -0300

    slicks implementados, navbar finalizada

---
+++ app/js/index.js
@@ -0,0 +1,17 @@
+$(".slick-header").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots:false,
+    infinite: true
+})
+
+$(".slick-projects").slick({
+    infinite: true,
+    arrows:true,
+    
+})
+
+$(".slick-header").slick({
+    infinite: true,
+})
