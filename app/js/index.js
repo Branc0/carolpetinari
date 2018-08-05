db8b73b4b600f75636ed7e8edbfef44463ad7018
branc0 <rrochadeazevedo@gmail.com>
2018-08-05 15:44:20 -0300

    feat: new text added

--- app/js/index.js
+++ app/js/index.js
@@ -9,9 +9,73 @@ $(".slick-header").slick({
 $(".slick-projects").slick({
     infinite: true,
     arrows:false,
+   // fade:true,
+   // speed:1000
 })
 
+$(".slick-construction").slick({
+    infinite: true,
+    arrows:true,
+    dots:true
+});
+
+$(".slick-myhouse").slick({
+    infinite: true,
+    arrows:true,
+    dots:true
+});
+
+$('.slick-portfolio').slick({
+    infinite: true,
+    arrows:true,
+    dots:true,
+    fade: true,
+})
+
+$('.a-collapse').click(function() {
+    if($(this).find("span").hasClass("rotate")){
+        $(this).find("span").removeClass("rotate");
+    }else{
+        $(this).find("span").addClass("rotate");
+    }
+});
+
+// smooth scroll
+$('a[href*="#"]').not('[href="#0"]').click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
 $(document).ready(function(){
+
+
 
 $(".menu-projects a").click(function(e){
     e.preventDefault();
